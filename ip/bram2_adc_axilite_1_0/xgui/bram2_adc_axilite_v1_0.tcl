ef973213e64cc36571513aa802f9b7ee4baf8f22
powersthegreat <96794311+powersthegreat@users.noreply.github.com>
2023-08-01 12:58:44 -0500

    adding bram2 with adc and axilite interfaces
    and adding ADC with only one output for testing

---
+++ ip/bram2_adc_axilite_1_0/xgui/bram2_adc_axilite_v1_0.tcl
@@ -0,0 +1,101 @@
+# Definitional proc to organize widgets for parameters.
+proc init_gui { IPINST } {
+  ipgui::add_param $IPINST -name "Component_Name"
+  #Adding Page
+  ipgui::add_page $IPINST -name "Page 0"
+
+  ipgui::add_param $IPINST -name "DATA_WIDTH"
+  ipgui::add_param $IPINST -name "ADDR_WIDTH" -widget comboBox
+  ipgui::add_param $IPINST -name "MEMORY_DEPTH" -widget comboBox
+
+}
+
+proc update_PARAM_VALUE.ADDR_WIDTH { PARAM_VALUE.ADDR_WIDTH } {
+	# Procedure called to update ADDR_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.ADDR_WIDTH { PARAM_VALUE.ADDR_WIDTH } {
+	# Procedure called to validate ADDR_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.DATA_WIDTH { PARAM_VALUE.DATA_WIDTH } {
+	# Procedure called to update DATA_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.DATA_WIDTH { PARAM_VALUE.DATA_WIDTH } {
+	# Procedure called to validate DATA_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.MEMORY_DEPTH { PARAM_VALUE.MEMORY_DEPTH } {
+	# Procedure called to update MEMORY_DEPTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.MEMORY_DEPTH { PARAM_VALUE.MEMORY_DEPTH } {
+	# Procedure called to validate MEMORY_DEPTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_S00_AXI_DATA_WIDTH { PARAM_VALUE.C_S00_AXI_DATA_WIDTH } {
+	# Procedure called to update C_S00_AXI_DATA_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S00_AXI_DATA_WIDTH { PARAM_VALUE.C_S00_AXI_DATA_WIDTH } {
+	# Procedure called to validate C_S00_AXI_DATA_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_S00_AXI_ADDR_WIDTH { PARAM_VALUE.C_S00_AXI_ADDR_WIDTH } {
+	# Procedure called to update C_S00_AXI_ADDR_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S00_AXI_ADDR_WIDTH { PARAM_VALUE.C_S00_AXI_ADDR_WIDTH } {
+	# Procedure called to validate C_S00_AXI_ADDR_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_S00_AXI_BASEADDR { PARAM_VALUE.C_S00_AXI_BASEADDR } {
+	# Procedure called to update C_S00_AXI_BASEADDR when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S00_AXI_BASEADDR { PARAM_VALUE.C_S00_AXI_BASEADDR } {
+	# Procedure called to validate C_S00_AXI_BASEADDR
+	return true
+}
+
+proc update_PARAM_VALUE.C_S00_AXI_HIGHADDR { PARAM_VALUE.C_S00_AXI_HIGHADDR } {
+	# Procedure called to update C_S00_AXI_HIGHADDR when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S00_AXI_HIGHADDR { PARAM_VALUE.C_S00_AXI_HIGHADDR } {
+	# Procedure called to validate C_S00_AXI_HIGHADDR
+	return true
+}
+
+
+proc update_MODELPARAM_VALUE.C_S00_AXI_DATA_WIDTH { MODELPARAM_VALUE.C_S00_AXI_DATA_WIDTH PARAM_VALUE.C_S00_AXI_DATA_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S00_AXI_DATA_WIDTH}] ${MODELPARAM_VALUE.C_S00_AXI_DATA_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.C_S00_AXI_ADDR_WIDTH { MODELPARAM_VALUE.C_S00_AXI_ADDR_WIDTH PARAM_VALUE.C_S00_AXI_ADDR_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S00_AXI_ADDR_WIDTH}] ${MODELPARAM_VALUE.C_S00_AXI_ADDR_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.DATA_WIDTH { MODELPARAM_VALUE.DATA_WIDTH PARAM_VALUE.DATA_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.DATA_WIDTH}] ${MODELPARAM_VALUE.DATA_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.ADDR_WIDTH { MODELPARAM_VALUE.ADDR_WIDTH PARAM_VALUE.ADDR_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.ADDR_WIDTH}] ${MODELPARAM_VALUE.ADDR_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.MEMORY_DEPTH { MODELPARAM_VALUE.MEMORY_DEPTH PARAM_VALUE.MEMORY_DEPTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.MEMORY_DEPTH}] ${MODELPARAM_VALUE.MEMORY_DEPTH}
+}
+
---
+++ ip/redpi_adc_2_0/redpi_adc_2/redpi_adc_2.srcs/sources_1/new/redpi_adc_2.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+
+
+module redpi_adc_2 (
+  input  wire adc_clk,
+  input  wire [13:0] adc_da_i,
+  input  wire [13:0] adc_db_i,
+  output wire signed [15:0] adc_da,
+//  output wire signed [15:0] adc_db,
+  output wire [1:0] adc_clk_o,
+  output wire adc_cdcs_o);
+
+//------------------------------------------------------------------//
+// ADC -------------------------------------------------------------//
+//------------------------------------------------------------------//
+
+  wire signed [15:0] adc_db;
+  
+  assign adc_clk_o = 2'b10; // generating ADC clock is disabled
+  assign adc_cdcs_o = 1'b1; //ADC clock duty cycle stabilizer is enabled
+
+  //sync adc input data to adc_clk
+  reg  [13:0] adc_db_raw = 0;
+  reg  [13:0] adc_da_raw = 0;
+
+  always@(posedge adc_clk) begin
+    adc_db_raw <= adc_db_i;
+    adc_da_raw <= adc_da_i;
+  end
+  assign adc_db = {{3{~adc_db_raw[13]}}, adc_db_raw[12:0]};
+  assign adc_da = {{3{~adc_da_raw[13]}}, adc_da_raw[12:0]};
+//------------------------------------------------------------------//
+
+endmodule
